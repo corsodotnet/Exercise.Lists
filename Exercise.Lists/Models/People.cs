f63d3cfc4a298acc39b5f3ec4d080088e3b54cff
Bruno Ferreira Rocha <bruno_ferreira@hotmail.it>
2022-04-27 09:43:00 +0200

    Utils Methods structure

---
+++ Exercise.Lists/Models/People.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercise.Lists.Models
+{
+    internal class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Exercise.Lists/Utils/OriginalTextFileProcessor.cs
@@ -0,0 +1,27 @@
+﻿using Exercise.Lists.Models;
+using System.Collections.Generic;
+
+namespace Exercise.Lists.Utils
+{
+    public static class OriginalTextFileProcessor
+    {
+        // load() PEOPLE objets from  FILE   
+        // --> extract lines from file  
+        // ->  create a List of objects from each column 
+        // -> each object must have PROPERTIES VALUES  taht match with each FILE COLUMN
+
+        // Save() people FILE from objects  
+
+
+        static void SaveToFile()
+        {
+
+        }
+        static List<People> ReadFromFile(string path)
+        {
+            List<People> data = new List<People>();
+
+            return data;
+        }
+    }
+}
