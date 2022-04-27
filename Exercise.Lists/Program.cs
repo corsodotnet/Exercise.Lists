0cfca885b98891b2d6f3762df56a1243f14a82a7
Bruno Ferreira Rocha <bruno_ferreira@hotmail.it>
2022-04-27 09:43:00 +0200

    Add project files.

---
+++ Exercise.Lists/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise.Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    // string path = "C:\temp\people.csv"
+
+
+            // 1 -  SAVE a list o PEOPLE  from DataList to file csv
+            // 2 -  Use the the same file to Load data from FILE to a List of PEOPLE 
+
+
+            // 3 -  Print out all the PEOPLE properties from file 
+
+            // use a utility class to create STANDARD METHODS to manager both situation !!
+            // USE FILE STATIC FILE PATH !!!
+        }
+
+        // Create a method to populate MockData
+    }
+}
---
+++ Exercise.Lists/Utils/OriginalTextFileProcessor.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise.Lists.Utils
+{
+    public static class OriginalTextFileProcessor
+    {
+        // load() PEOPLE objets from  FILE   
+        // --> extract lines from file  
+        // ->  create a List of objects from each column 
+        // -> each object must have PROPERTIES VALUES  taht match with each FILE COLUMN
+
+        // Save() people FILE from objects  
+
+
+        static void ComposeFile()
+        {
+
+        }
+    }
+}
