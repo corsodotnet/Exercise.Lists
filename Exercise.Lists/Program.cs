512f0f6e38095444295ffc4fd853ffd2743b24e2
Bruon Ferreira Rocha <bruon.ferreira.rocha@gmail.com>
2022-05-04 17:00:29 +0200

    edit

--- Exercise.Lists/Program.cs
+++ Exercise.Lists/Program.cs
@@ -6,16 +6,7 @@ namespace Exercise.Lists
     internal class Program
     {
         static void Main(string[] args)
-        {    // string path = "C:\temp\people.csv"
-
-
-            // 1 -  SAVE a list o PEOPLE  from DataList to file csv
-            // 2 -  Use the the same file to Load data from FILE to a List of PEOPLE 
-
-            //foreach (var p in newPeople)
-            //{
-            //    Console.WriteLine($"{ p.FirstName } { p.LastName } (IsAlive = { p.IsAlive })");
-            //}
+        {   
         }
         private static void PopulateLists(List<Person> people)
         {
